f73b45042ef9454dc628876e49beef09350e3e87
Erickson <s1010541@ee.yzu.edu.tw>
2014-06-21 03:49:10 +0800

    add navbar, homepage

--- application/views/_templates/header.php
+++ application/views/_templates/header.php
@@ -7,7 +7,7 @@
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- css -->
-    <link rel="stylesheet" href="<?php echo URL; ?>public/css/bootstrap.min .css">
+    <link rel="stylesheet" href="<?php echo URL; ?>public/css/bootstrap.min.css">
     <link rel="stylesheet" href="<?php echo URL; ?>public/css/bootflat.min.css">
     <!-- jQuery -->
     <script src="//code.jquery.com/jquery-2.1.1.min.js"></script>
@@ -15,3 +15,25 @@
 </head>
 <body>
 <!-- header -->
+<nav class="navbar navbar-default navbar-inverse " role="navigation">
+    <div class="container">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="<?php echo URL; ?>">YzuEESA 元智電機系學會</a>
+            </div>
+            <div class="collapse navbar-collapse" id="navbar-collapse">
+                <ul class="nav navbar-nav">
+                    <li class="active">
+                            <a href="#">iStudy</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</nav>
---
+++ application/views/home/index.php
@@ -0,0 +1,8 @@
+<div class="jumbotron" id="banner" style="background-position: 50% 0px;">
+    <div class="container">
+        <div id="banner-title">
+            <h1>YzuEESA 元智大學電機系學會</h1>
+            <h1>電機系學生不可錯過系上大大小小的活動</h1>
+        </div>
+    </div>
+</div>
