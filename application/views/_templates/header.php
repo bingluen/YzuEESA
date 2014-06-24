acf5e78a491b0c8f5a9e9c599c17f221beb0f23d
Erickson <s1010541@ee.yzu.edu.tw>
2014-06-24 13:09:02 +0800

    update header & homepage

--- application/views/_templates/header.php
+++ application/views/_templates/header.php
@@ -1,39 +1,51 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>YzuEESA 元智電機系學會</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- jQuery -->
+    <script src="<?php echo URL; ?>public/js/jquery-1.10.2.js"></script>
+    <!--<script src="<?php echo URL; ?>public/js/jquery-ui-1.10.4.min.js"></script>-->
+    <script src="<?php echo URL; ?>public/js/bootstrap.min.js"></script>
+    <!-- our JavaScript -->
     <!-- css -->
     <link rel="stylesheet" href="<?php echo URL; ?>public/css/bootstrap.min.css">
     <link rel="stylesheet" href="<?php echo URL; ?>public/css/bootflat.min.css">
-    <!-- jQuery -->
-    <script src="//code.jquery.com/jquery-2.1.1.min.js"></script>
-    <!-- our JavaScript -->
+    <!--<link rel="stylesheet" href="<?php echo URL; ?>public/css/ui-lightness/jquery-ui-1.10.4.min.css">-->
+    <!-- our css -->
+    <link rel="stylesheet" href="<?php echo URL; ?>public/css/yzueesa.css">
 </head>
 <body>
-<!-- header -->
-<nav class="navbar navbar-default navbar-inverse " role="navigation">
-    <div class="container">
-        <div class="container-fluid">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="<?php echo URL; ?>">YzuEESA 元智電機系學會</a>
-            </div>
-            <div class="collapse navbar-collapse" id="navbar-collapse">
-                <ul class="nav navbar-nav">
-                    <li class="active">
-                            <a href="#">iStudy</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
+<div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+
+    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#header-collapse">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+    </button>
+
+    <div class="navbar-header">
+        <a class="navbar-brand" href="#photoslide" id="bar-brand">YzuEESA 元智電機系學會</a>
     </div>
-</nav>
+
+    <div class="collapse navbar-collapse" id="header-collapse">
+        <ul class="nav navbar-nav">
+            <li><a href="<?php echo URL; ?>">首頁</a></li>
+            <li><a href="">關於學會</a></li>
+            <li><a href="">學會活動</a></li>
+            <li><a href="">精彩回顧</a></li>
+            <li><a href="">iStudy Project</a></li>
+            <li><a href="">公開資訊</a></li>
+            <li><a href="">問題聯繫</a></li>
+        </ul>
+        <ul class="nav navbar-nav navbar-right" id="fblog">
+            <li><a href="https://www.facebook.com/yzueesa" target="_blank"><span class="fblogo"></span></a></li>
+        </ul>
+    </div>
+
+</div>
+<!-- header -->
--- application/views/home/index.php
+++ application/views/home/index.php
@@ -1,8 +1,45 @@
-<div class="jumbotron" id="banner" style="background-position: 50% 0px;">
-    <div class="container">
-        <div id="banner-title">
-            <h1>YzuEESA 元智大學電機系學會</h1>
-            <h1>電機系學生不可錯過系上大大小小的活動</h1>
+<div id="photoslide" class="carousel slide" data-ride="carousel">
+
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+        <li data-target="#photoslide" data-slide-to="0" class="active"></li>
+        <li data-target="#photoslide" data-slide-to="1"></li>
+
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+
+        <div class="item active">
+            <img src="<?php echo URL; ?>public/img/test/1.png">
+            <div class="carousel-caption">
+                <h1>玩樂</h1>
+                <h4>第一屆電機系魁地奇大賽</h4>
+            </div>
+        </div>
+
+        <div class="item">
+            <img src="<?php echo URL; ?>public/img/test/2.png">
+            <div class="carousel-caption">
+                <h1>學習</h1>
+                <h4>iStudy 讀書會</h4>
+            </div>
+        </div>
+
+</div>
+
+<div class="container">
+    <div class="row">
+        <div class="col-sm-6 col-md-4">
+            <div class="thumbnail">
+            <img data-src="holder.js/300x200" alt="...">
+            <div class="caption">
+                <h3></h3>
+                <p>...</p>
+                <p><a href="">More...</p>
+            </div>
         </div>
     </div>
+</div>
+
 </div>
