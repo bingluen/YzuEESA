c008bba8c6ada35b9a8bdfb653df58c07d66c2a9
Erickson <s1010541@ee.yzu.edu.tw>
2014-07-30 17:11:18 +0800

    fix and change items UI

--- application/views/manager/cash_flow/items_man.php
+++ application/views/manager/cash_flow/items_man.php
@@ -1,64 +1,49 @@
 <div class="tab-content panel" id="man-content">
     <div class="tab-pane fade active in">
-    <div class="panel-group" id="project-group">
     <?php
-    $first = true;
     foreach ($data['project'] as $project) { ?>
-        <div class="panel panel-default">
-            <div class="panel-heading">
-                <h4 class="panel-title">
-                <a data-toggle="collapse" data-parent="#project-group" href="#project-<?=$project->project_id; ?>">
-                    ＃<?=$project->project_name; ?> <div class="text-right" style="display:inline;"><small>計畫負責人：<?=$project->project_host; ?></small></div>
-                </h4>
-                </a>
+            <h4>
+                ＃<?=$project->project_name; ?><small>計畫負責人：<?=$project->project_host; ?></small>
+            </h4>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>類型</th>
+                        <th>項目</th>
+                        <th>金額</th>
+                        <th>申請人</th>
+                        <th>狀態</th>
+                        <th>審核人</th>
+                        <th>申請時間</th>
+                        <th>核准時間</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                        foreach ($data['items'][$project->project_id] as $item) {
+                    ?>
+                    <tr>                                <td><input class="checkbox" type="checkbox" name="items-<?=$item->items_project; ?>-select" value="<?=$item->items_id;?>"></td>
+                        <td><?=($item->items_outlay === 'T') ? '<span class="label label-warning">支出</span>':'<span class="label label-info">收入</span>';?></td>
+                        <td><?=$item->items_name;?></td>
+                        <td><?=$item->items_price;?></td>
+                        <td><?=$item->items_applicant;?></td>
+                        <td><?=($item->items_status === '0') ? '<span class="label label-default">尚未審核</span>':(($item->items_status === '1')? '<span class="label label-success">已核准</span>':'<span class="label label-danger">不核准</span>');?></td>
+                        <td><?=($item->items_reviewer === NULL) ? '--' : $item->items_reviewer;?></td>
+                        <td><?=$item->items_app_time;?></td>
+                        <td><?=($item->items_status === '0') ? '--':$item->items_rev_time;?></td>
+                    </tr>
+                    <?php } ?>
+                </tbody>
+            </table>
+            <div class="text-right">
+                    所有選取的申請項目
+                    <button id="items-<?=$project->project_id; ?>-pass-btn" type="button" class="btn btn-sm btn-success">通過</button>
+                    <button id="items-<?=$project->project_id; ?>-notpass-btn" type="button" class="btn btn-sm btn-danger">不通過</button>
             </div>
-            <div id="project-<?=$project->project_id; ?>" class="panel-collapse collapse <?php if($first) echo 'in';?>">
-                <div class="panel-body">
-                    <table class="table">
-                        <thead>
-                            <tr>
-                                <th>#</th>
-                                <th>類型</th>
-                                <th>項目</th>
-                                <th>金額</th>
-                                <th>申請人</th>
-                                <th>狀態</th>
-                                <th>審核人</th>
-                                <th>申請時間</th>
-                                <th>核准時間</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <?php
-                                foreach ($data['items'][$project->project_id] as $item) {
-                            ?>
-                            <tr>
-                                <td><input class="checkbox" type="checkbox" name="items-<?=$item->items_project; ?>-select" value="<?=$item->items_id;?>"></td>
-                                <td><?=($item->items_outlay === 'T') ? '<span class="label label-warning">支出</span>':'<span class="label label-info">收入</span>';?></td>
-                                <td><?=$item->items_name;?></td>
-                                <td><?=$item->items_price;?></td>
-                                <td><?=$item->items_applicant;?></td>
-                                <td><?=($item->items_status === '0') ? '<span class="label label-default">尚未審核</span>':(($item->items_status === '1')? '<span class="label label-success">已核准</span>':'<span class="label label-danger">不核准</span>');?></td>
-                                <td><?=($item->items_reviewer === NULL) ? '--' : $item->items_reviewer;?></td>
-                                <td><?=$item->items_app_time;?></td>
-                                <td><?=($item->items_status === '0') ? '--':$item->items_rev_time;?></td>
-                            </tr>
-                            <?php } ?>
-                        </tbody>
-                    </table>
-                    <div class="text-right">
-                        所有選取的申請項目
-                        <button id="items-<?=$project->project_id; ?>-pass-btn" type="button" class="btn btn-sm btn-success">通過</button>
-                        <button id="items-<?=$project->project_id; ?>-notpass-btn" type="button" class="btn btn-sm btn-danger">不通過</button>
-                    </div>
-                </div>
-            </div>
-        </div>
     <?php
-    $first = false;
      } ?>
     </div>
-</div>
 </div>
 
 <script type="text/javascript">
--- application/views/manager/sidebar.php
+++ application/views/manager/sidebar.php
@@ -1,3 +1,17 @@
+<ul class="panel nav nav-tabs nav-justified"  id="man-sidebar">
+    <li <?php if($data == 'home') echo 'class="active"'; ?>><a href="">管理首頁</a></li>
+    <li <?php if($data == 'CashFlow') echo 'class="active"'; ?>><a href="#" id="cashflow_tab" class="dropdown-toggle" data-toggle="dropdown">金流系統 <b class="caret"></b></a>
+        <ul class="dropdown-menu" role="menu" aria-labelledby="myTabDrop1">
+            <li><a href="<?php echo URL ?>webMan/CashFlow/Project" tabindex="-1" data-toggle="cashflow_tab">Project</a></li>
+            <li><a href="<?php echo URL ?>webMan/CashFlow/Items" tabindex="-1" data-toggle="cashflow_tab">Items</a></li>
+        </ul>
+    </li>
+    <li <?php if($data == 'Worker') echo 'class="active"'; ?>><a href="<?php echo URL ?>webMan/CashFlow/Worker">工人帳號</a></li>
+    <li><a href="">登出系統</a></li>
+</ul>
+
+
+<!--
 <ul class="panel nav nav-tabs" id="man-sidebar">
     <li <?php if($data == 'home') echo 'class="active"'; ?>><a href="">管理首頁</a></li>
     <li <?php if($data == 'CashFlow') echo 'class="active"'; ?>><a href="">金流系統</a></li>
@@ -5,6 +19,7 @@
     <li><a href="">登出系統</a></li>
 </ul>
 
+    流動版面廢棄
 <ul class="panel nav nav-tabs nav-justified"  id="man-sidebar-max1023">
     <li <?php if($data == 'home') echo 'class="active"'; ?>><a href="">管理首頁</a></li>
     <li <?php if($data == 'CashFlow') echo 'class="active"'; ?>><a href="#" id="cashflow_tab" class="dropdown-toggle" data-toggle="dropdown">金流系統 <b class="caret"></b></a>
@@ -16,3 +31,4 @@
     <li <?php if($data == 'Worker') echo 'class="active"'; ?>><a href="">工人帳號</a></li>
     <li><a href="">登出系統</a></li>
 </ul>
+-->
