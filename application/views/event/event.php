c0fd3b094b1655339dbcf36ff86962796c5a13d7
Erickson <a820224@gmail.com>
2014-08-21 03:51:11 +0800

    implement event page of activities

---
+++ application/views/event/event.php
@@ -0,0 +1,6 @@
+<div id="main" class="container">
+    <ol class="breadcrumb breadcrumb-arrow">
+        <li><a href="<?=URL?>"><i class="glyphicon glyphicon-home"></i> 首頁</a></li>
+        <li><a href="<?=URL?>Information/"><i class="glyphicon glyphicon-bullhorn"></i> 學會活動</a></li>
+    </ol>
+</div>
