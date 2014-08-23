9e809602ca0de882fe36208201d49c61e239830a
Erickson <s1010541@ee.yzu.edu.tw>
2014-08-24 05:24:32 +0800

    structure update

--- structure.sql
+++ structure.sql
@@ -1,20 +1,14 @@
--- phpMyAdmin SQL Dump
+﻿-- phpMyAdmin SQL Dump
 -- version 4.1.14.3
 -- http://www.phpmyadmin.net
 --
 -- 主機: localhost
--- 產生時間： 2014-08-22: 19:38:15
+-- 產生時間： 2014-08-23: 23:20:38
 -- 伺服器版本: 5.6.17
 -- PHP 版本： 5.5.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
 
 --
 -- 資料庫： `yzueesa`
@@ -134,7 +128,3 @@ CREATE TABLE IF NOT EXISTS `worker_class` (
   UNIQUE KEY `class_level` (`class_level`),
   KEY `class_name` (`class_name`,`class_level`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
